c403f292d079332202543abeed71e081f1af56c0
shenwei5860771 <286615510@qq.com>
2017-11-12 21:45:44 +0800

    aa

--- mall/mall_admin/src/main/java/com/mall/controller/UserController.java
+++ mall/mall_admin/src/main/java/com/mall/controller/UserController.java
@@ -29,6 +29,9 @@ public class UserController {
 			Order orderByOderId = orderService.getOrderByOderId("111");
 			Order orderByOderId2 = orderService.getOrderByOderId("111");
 			System.out.println("sssssswwwwwwww");
+			System.out.println("sssssswwwwwwww");
+			System.out.println("sssssswwwwwwww");
+			System.out.println("sssssswwwwwwww");
 		} catch (Exception e) {
 			logger.error(e);
 		}
