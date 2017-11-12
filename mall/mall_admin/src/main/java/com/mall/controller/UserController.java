b6527c0045d0657ab8c898b27f12cd304bcaf376
lspaccount-github <lspaccount_wy@163.com>
2017-11-12 21:28:30 +0800

    rr

--- mall/mall_admin/src/main/java/com/mall/controller/UserController.java
+++ mall/mall_admin/src/main/java/com/mall/controller/UserController.java
@@ -27,7 +27,8 @@ public class UserController {
 	public String getUsers(ModelMap modelMap,HttpServletRequest request,HttpServletResponse response){
 		try {
 			Order orderByOderId = orderService.getOrderByOderId("111");
-			Order orderByOderId2 = orderService.getOrderByOderId("111");
+			Order orderByOderId3 = orderService.getOrderByOderId("111");
+			System.out.println("aa");
 		} catch (Exception e) {
 			logger.error(e);
 		}
